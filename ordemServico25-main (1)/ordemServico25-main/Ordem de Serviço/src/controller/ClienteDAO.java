18939b7c923e715e79f8939afac98e91170c1e64
lmarques0 <l.marques@estudante.ifmt.edu.br>
2025-10-24 13:25:25 -0300

    24/10/2025

---
+++ ordemServico25-main (1)/ordemServico25-main/Ordem de Serviço/src/controller/ClienteDAO.java
@@ -0,0 +1,50 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller;
+
+import java.util.List;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import model.Cliente;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+/**
+ *
+ * @author clebe
+ */
+public class ClienteDAO {
+    public List<Cliente> listarCliente() {
+        try {
+
+            //1 passo criar a lista
+            List<Cliente> lista = new ArrayList<>();
+
+            //2 passo - criar o sql , organizar e executar.
+            String sql = "select * from tbclientes";
+            PreparedStatement stmt = conexao.prepareStatement(sql);
+            ResultSet rs = stmt.executeQuery();
+
+            while (rs.next()) {
+                Cliente obj = new Cliente();
+
+                obj.setId(rs.getInt("idcli"));
+                obj.setNome(rs.getString("nomecli"));
+                obj.setEndereco(rs.getString("endcli"));
+                obj.setEndereco(rs.getString("fonecli"));
+                obj.setEmail(rs.getString("emailcli"));
+                lista.add(obj);
+            }
+
+            return lista;
+
+        } catch (SQLException erro) {
+
+            JOptionPane.showMessageDialog(null, "Erro :" + erro);
+            return null;
+        }
+
+    }
+}
